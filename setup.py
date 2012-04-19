e2ba1b97e6acc359d5ffdc736ca6cd8e601b912d
Theron Luhn <theron@luhn.com>
2012-04-19 11:45:28 -0700

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='formula',
+    version='1.0',
+    description='',
+    author='Theron Luhn',
+    author_email='theron@luhn.com',
+    packages=find_packages(),
+    requires=[
+    ],
+    )
