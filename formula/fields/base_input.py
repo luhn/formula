3767775b86077f6d581894fab0b70522da105942
Theron Luhn <theron@luhn.com>
2012-04-21 15:30:46 -0700

    Added id attribute to <input> tags.

--- formula/fields/base_input.py
+++ formula/fields/base_input.py
@@ -3,7 +3,10 @@ from field import Field
 class Input(Field):
     def __html__(self):
         """Render the <input> tag."""
-        r = ['<input type="', self.type, '" name="', self.name, '"']
+        r = ['<input type="', self.type, '" name="', self.name, '" id="']
+        if self.parent_name:
+            r.extend([self.parent_name, '_'])
+        r.extend([self.name, '"'])
         if self.placeholder:
             r.extend([' placeholder="', self.escape(self.placeholder), '"'])
         if self.value:
