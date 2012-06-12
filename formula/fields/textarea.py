cc765aff01679acb6568b93af9976a235c5674de
Theron Luhn <theron@luhn.com>
2012-06-12 14:46:41 -0500

    Improved textarea field

--- formula/fields/textarea.py
+++ formula/fields/textarea.py
@@ -3,19 +3,18 @@ from field import Field
 class Textarea(Field):
     """Class for a textarea field"""
 
-    def __init__(self, *args, **kwargs):
-        if 'rows' in kwargs:
-            self.rows = kwargs['rows']
-        else:
-            self.rows = 3
-        Field.__init__(self, *args, **kwargs)
+    def __init__(self, name, cols=25, rows=3, **kwargs):
+        self.cols = cols
+        self.rows = rows
+        Field.__init__(self, name, **kwargs)
 
     def __html__(self):
         """Render the <input> tag."""
         r = ['<textarea name="', self.name, '" id="']
         if self.parent_name:
             r.extend([self.parent_name, '_'])
-        r.extend([self.name, '" rows="', str(self.rows), '">'])
+        r.extend([self.name, '" rows="', str(self.rows), '" cols="',
+            str(self.cols), '">'])
         if self.value:
             r.append(self.escape(str(self.value)))
         r.append('</textarea>')
