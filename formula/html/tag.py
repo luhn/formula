ecca89d09121a5e3866e59e9cfe9c12053678098
Theron Luhn <theron@luhn.com>
2013-01-04 09:49:15 -0600

    Basic attributes.

--- formula/html/tag.py
+++ formula/html/tag.py
@@ -9,13 +9,16 @@ class Tag(object):
     #: Enables XML-style HTML.
     XHTML = True
 
+    #: The regex checked against the tag name and attributes.
     valid_attr_regex = re.compile('[a-zA-Z_:][-a-zA-Z0-9_:.]*$')
 
-    def __init__(self, tag_name):
+    def __init__(self, tag_name, **kwargs):
         """Construct an object representing an HTML tag.
 
-        :param tag_name: The HTML tag name
+        :param tag_name: The HTML tag name.  Must match valid_attr_regex.
         :type tag_name: str
+        :param **kwargs:  All the additional keyword arguments will be set
+            as attributes.
 
         """
 
@@ -24,10 +27,50 @@ class Tag(object):
 
         self.tag_name = tag_name.lower()
         self.self_closing = tag_name in void_elements
+        self.attributes = {}
+
+        #Set all the attributes passed in.
+        for (key, value) in kwargs.iteritems():
+            self.set_attribute(key, value)
+
+    def set_attribute(self, name, value):
+        """Set a single attribute to value.
+
+        :param name:  The attribute name.  Must match valid_attr_regex.
+        :type name:  str
+        :param value:  The value for the attribute.  Double quotes will
+            automatically be escaped.
+        :type value:  str
+
+        """
+
+        if not re.match(self.valid_attr_regex, name):
+            raise ValueError('"' + name +'" is an invalid attribute name.')
+
+        self.attributes[name.lower()] = value
+
+
+    def set_attributes(self, attributes):
+        """Set multiple attributes.
+
+        :param attributes:  A dictionary of the attributes.
+        :type attributes:  dict
+
+        """
+
+        for name, value in attributes.iteritems():
+            self.set_attribute(name, value)
+
 
     def render(self):
         """Render the object into a string of HTML."""
         r = ['<', self.tag_name ]
+
+        #Put in the attributes
+        for (name, value) in self.attributes.iteritems():
+            r.extend([' ', name, '="', escape_quotes(value), '"'])
+
+        #Close the tag
         if self.self_closing:
             if self.XHTML:
                 r.append(' />')
@@ -35,10 +78,17 @@ class Tag(object):
                 r.append('>')
         else:
             r.extend(['></', self.tag_name, '>'])
+
+        #Smoosh it all together and return
         return ''.join(r)
+
 
     def __html__(self):
         """An alias of render.  For use with templating engines."""
         return self.render()
+
+
+def escape_quotes(value):
+    return value.replace('"', '&quot;')
 
 
--- tests/html.py
+++ tests/html.py
@@ -1,4 +1,5 @@
 import unittest
+from bs4 import BeautifulSoup
 from formula.html import Tag
 
 class TestHtmlClasses(unittest.TestCase):
@@ -31,4 +32,59 @@ class TestHtmlClasses(unittest.TestCase):
                 '<img>')
 
         Tag.XHTML = True #Reset
+
+
+    def test_init_attributes(self):
+        self.assertEqual(
+                Tag('img', SRC='http://i.imgur.com/iWHK2.gif').render(),
+                '<img src="http://i.imgur.com/iWHK2.gif" />')
+
+        #Now we need to start parsing the HTML, because the attributes are
+        #stored in a dict, the order of which is not consistant across all
+        #platforms and implementations.
+        tag = Tag('img', src='http://i.imgur.com/iWHK2.gif',
+                alt='Cats are liquid, not solid.')
+        soup = BeautifulSoup(tag.render())
+
+        self.assertEqual(soup.img.name, 'img')
+        self.assertEqual(soup.img['src'], 'http://i.imgur.com/iWHK2.gif')
+        self.assertEqual(soup.img['alt'], 'Cats are liquid, not solid.')
+
+    def test_set_attribute(self):
+        #Now we'll repeat the test, using the set_attribute method.
+        tag = Tag('img')
+        tag.set_attribute('src', 'http://i.imgur.com/iWHK2.gif')
+        tag.set_attribute('alt', 'Cats are liquid, not solid.')
+        soup = BeautifulSoup(tag.render())
+
+        self.assertEqual(soup.img.name, 'img')
+        self.assertEqual(soup.img['src'], 'http://i.imgur.com/iWHK2.gif')
+        self.assertEqual(soup.img['alt'], 'Cats are liquid, not solid.')
+
+    def test_set_attributes(self):
+        #And then the set_attributes method
+        tag = Tag('img')
+        tag.set_attributes({
+            'src': 'http://i.imgur.com/iWHK2.gif',
+            'alt': 'Cats are liquid, not solid.'
+            })
+        soup = BeautifulSoup(tag.render())
+
+        self.assertEqual(soup.img.name, 'img')
+        self.assertEqual(soup.img['src'], 'http://i.imgur.com/iWHK2.gif')
+        self.assertEqual(soup.img['alt'], 'Cats are liquid, not solid.')
+
+
+    def test_invalid_attributes(self):
+        tag = Tag('img')
+        with self.assertRaises(ValueError):
+            tag.set_attribute('src`', 'http://i.imgur.com/iWHK2.gif')
+
+
+    def test_escaping_attribute_values(self):
+        self.assertEqual(
+                Tag('a', href='<">').render(),
+                '<a href="<&quot;>"></a>')
+
+
 
